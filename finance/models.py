362653cb92a87bf0f676f7248cb22847765f2b71
Pradeep Yadav <pkyisky@gmail.com>
2016-11-23 10:21:50 +0530

    finance tabs ui started

--- finance/models.py
+++ finance/models.py
@@ -1,5 +1,43 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
+from ERP.models import service
 
 # Create your models here.
+
+def getInvoicesPath(instance , filename ):
+    return 'finance/invoices/%s_%s_%s' % (str(time()).replace('.', '_'),instance.user.username, filename)
+
+class Transaction(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    fromAcc = models.PositiveIntegerField()
+    fromIFSC = models.CharField(max_length = 10)
+    toAcc = models.PositiveIntegerField()
+    toIFSC = models.CharField(max_length = 10)
+    ammount = models.PositiveIntegerField()
+    user = models.ForeignKey(User , related_name='transactions' , null = False)
+
+CURRENCY_CHOICES = (
+    ('INR' , 'INR'),
+    ('USD' , 'USD'),
+)
+
+class ExpenseSheet(models.Model):
+    user = models.ForeignKey(User , related_name='expenseGeneratedOrSubmitted' , null = False)
+    created = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default = False)
+    approvalMatrix = models.PositiveSmallIntegerField(default=1)
+    approvalStage = models.PositiveSmallIntegerField(default=0)
+    dispensed = models.BooleanField(default = False)
+    settlement = models.ForeignKey(Transaction , null = True)
+
+
+class Invoice(models.Model):
+    user = models.ForeignKey(User , related_name='invoiceGeneratedOrSubmitted' , null = False)
+    created = models.DateTimeField(auto_now_add=True)
+    service = models.ForeignKey(service , null = False)
+    amount = models.PositiveIntegerField(null=False , default=0)
+    currency = models.CharField(max_length = 5 , choices = CURRENCY_CHOICES)
+    dated = models.DateTimeField(null = False)
+    attachment = models.FileField(upload_to = getInvoicesPath ,  null = True)
+    sheet = models.ForeignKey(ExpenseSheet , related_name='invoices' , null = False)
--- finance/static/js/app.finance.js
+++ finance/static/js/app.finance.js
@@ -1,1 +1,29 @@
 // you need to first configure the states for this app
+
+app.config(function($stateProvider){
+
+  $stateProvider
+  .state('businessManagement.finance', {
+    url: "/GIT",
+    views: {
+       "": {
+          templateUrl: '/static/ngTemplates/genericAppBase.html',
+       },
+       "menu@businessManagement.finance": {
+          templateUrl: '/static/ngTemplates/genericMenu.html',
+          controller : 'controller.generic.menu',
+        },
+        "@businessManagement.finance": {
+          templateUrl: '/static/ngTemplates/app.finance.default.html',
+          controller : 'businessManagement.finance.default',
+        }
+    }
+  })
+});
+
+
+app.controller('businessManagement.finance.default' , function($scope , $http , $aside , $state, Flash , $users , $filter , $permissions){
+  // settings main page controller
+
+
+})
