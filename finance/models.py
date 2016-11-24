ba8b06120778450e1f65edbbf519c060f01f51df
pradeep <pkyisky@gmail.com>
2016-11-24 19:52:46 +0530

    finance models

--- finance/models.py
+++ finance/models.py
@@ -2,18 +2,33 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 from ERP.models import service
-
+from projects.models import project
 # Create your models here.
 
 def getInvoicesPath(instance , filename ):
     return 'finance/invoices/%s_%s_%s' % (str(time()).replace('.', '_'),instance.user.username, filename)
 
-class Transaction(models.Model):
+class Account(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    fromAcc = models.PositiveIntegerField()
-    fromIFSC = models.CharField(max_length = 10)
-    toAcc = models.PositiveIntegerField()
-    toIFSC = models.CharField(max_length = 10)
+    number = models.PositiveIntegerField()
+    ifsc = models.CharField(max_length = 15 , null = False)
+    bank = models.CharField(max_length = 50 , null = False)
+    bankAddress = models.TextField(max_length = 500 , null = False)
+    contactPerson = models.ForeignKey(User , null = False , related_name = 'accountsManaging')
+    authorizedSignaturies = models.ManyToManyField(User , related_name = 'checkingAccounts')
+
+
+class CostCenter(models.Model):
+    head = models.ForeignKey(User , null = False)
+    name = models.CharField(max_length = 100 , blank = False)
+    code = models.CharField(max_length = 50 , blank = False)
+    created = models.DateTimeField(auto_now_add=True)
+    account = models.ForeignKey(Account)
+
+
+class Transaction(models.Model):
+    fromAcc = models.ForeignKey(Account , null = False , related_name = 'debits')
+    toAcc = models.ForeignKey(Account , null = False , related_name = 'credits')
     ammount = models.PositiveIntegerField()
     user = models.ForeignKey(User , related_name='transactions' , null = False)
 
@@ -30,7 +45,8 @@ class ExpenseSheet(models.Model):
     approvalStage = models.PositiveSmallIntegerField(default=0)
     dispensed = models.BooleanField(default = False)
     settlement = models.ForeignKey(Transaction , null = True)
-
+    notes = models.TextField(max_length = 500 , null = True)
+    project = models.ForeignKey(project , null = False)
 
 class Invoice(models.Model):
     user = models.ForeignKey(User , related_name='invoiceGeneratedOrSubmitted' , null = False)
--- finance/static/js/app.finance.expenses.js
+++ finance/static/js/app.finance.expenses.js
@@ -1,5 +1,31 @@
 app.controller('businessManagement.finance.expenses' , function($scope , $http , $aside , $state, Flash , $users , $filter , $permissions){
   // settings main page controller
+  views = [{name : 'list' , icon : 'fa-th-large' ,
+      template : '/static/ngTemplates/genericTable/genericSearchList.html' ,
+      itemTemplate : '/static/ngTemplates/app.GIT.groups.item.html',
+    },
+  ];
+
+  var options = {
+    main : {icon : 'fa-pencil', text: 'edit'} ,
+    };
+
+  $scope.config = {
+    views : views,
+    url : '/api/finance/expenseSheet/',
+    searchField: 'title',
+    deletable : true,
+    editorTemplate : '/static/ngTemplates/app.GIT.form.groups.html',
+    canCreate : true,
+    itemsNumPerView : [12,24,48],
+  }
+
+
+  $scope.tableAction = function(target , action , mode){
+    console.log(target , action , mode);
+    console.log($scope.data.tableData);
+
+  }
 
 
 })
