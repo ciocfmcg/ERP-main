c0052368f8c5a9e25ec7418e4d3ea83e3f55e674
himanshu <himanshu@cioc.co.in>
2017-05-22 19:26:43 +0530

    notes rework started

--- PIM/static/js/app.notes.js
+++ PIM/static/js/app.notes.js
@@ -4,11 +4,20 @@ app.controller("controller.home.notes", function($scope , $state , $users ,  $st
   $scope.canvas = new fabric.Canvas('canvas');
   $scope.canvas.selection = true;
   $scope.canvas.isDrawingMode = false;
-
+  $scope.fontName = 'Times New Roman';
   $scope.bookInView = -1;
   $scope.pageInView = -1;
   $scope.notebooks = [];
+  var flag1 = true;
+  var flag2 = true;
+  var flag3 = true;
 
+  $scope.$watch('fontName', function(newValue , oldValue) {
+    var selectedObj = $scope.fontName;
+    selectedObj.fontFamily = newValue;
+  });
+
+  //var temp = false;
   $scope.$watch('bookInView' , function(newValue , oldValue){
     if (newValue != -1) {
       if ($scope.notebooks[newValue].pages.length == 0) {
@@ -97,20 +106,40 @@ app.controller("controller.home.notes", function($scope , $state , $users ,  $st
       top: e.layerY ,
       fontSize:14,
     });
-    // newText.set('selectable', true);
-    // console.log(newText);
     $scope.canvas.add(newText);
     $scope.canvas.setActiveObject(newText);
     newText.enterEditing();
     newText.hiddenTextarea.focus();
 
   }
-  $scope.canvas.on('mouse:down', function(options) {
-    if (!$scope.canvas.isDrawingMode){
-      $scope.addText(options.e);
-    }
+
+  $scope.canvas.on('object:selected',function(e){
+
+    obj = e.target;
+    console.log(obj);
+    flag1 = false;
+      if(obj.get('type') == "i-text"){
+      $scope.showTextOptions = true;
+  }
+  else
+    $scope.showTextOptions = false;
+  $scope.showDeleteOption = true;
+  });
+  $scope.canvas.on('selection:cleared',function(e){
+    flag1 = true;
+    $scope.showTextOptions = false;
+    $scope.showDeleteOption = false;
   });
 
+  $scope.canvas.on('mouse:down', function(options) {
+
+    if (!$scope.canvas.isDrawingMode && flag1==true){
+      console.log(options.e);
+      $scope.addText(options.e);
+      $scope.showTextOptions = false;
+      $scope.showDeleteOption = false;
+    }
+  });
 
   window.addEventListener('resize', resizeCanvas, false);
 
@@ -123,8 +152,12 @@ app.controller("controller.home.notes", function($scope , $state , $users ,  $st
   // resize on init
   resizeCanvas();
 
+  $scope.showTextOptions = false;
+  $scope.showDeleteOption = false;
   $scope.canvas.on('object:moving', function (e) {
     var obj = e.target;
+
+    console.log(obj);
      // if object is too big ignore
     if(obj.currentHeight > obj.canvas.height || obj.currentWidth > obj.canvas.width){
       return;
@@ -134,14 +167,102 @@ app.controller("controller.home.notes", function($scope , $state , $users ,  $st
     if(obj.getBoundingRect().top < 0 || obj.getBoundingRect().left < 0){
       obj.top = Math.max(obj.top, obj.top-obj.getBoundingRect().top);
       obj.left = Math.max(obj.left, obj.left-obj.getBoundingRect().left);
+
     }
     // bot-right corner
     if(obj.getBoundingRect().top+obj.getBoundingRect().height  > obj.canvas.height || obj.getBoundingRect().left+obj.getBoundingRect().width  > obj.canvas.width){
       obj.top = Math.min(obj.top, obj.canvas.height-obj.getBoundingRect().height+obj.top-obj.getBoundingRect().top);
       obj.left = Math.min(obj.left, obj.canvas.width-obj.getBoundingRect().width+obj.left-obj.getBoundingRect().left);
     }
+    //temp = true;
   });
 
 
+  $scope.addImage = function() {
+    fabric.Image.fromURL('/static/images/brandLogo.jpg', function(img) {
+      img.scale(0.5).set({
+        left: 100,
+        top: 100,
+        angle: -15,
+      });
+      $scope.canvas.add(img).setActiveObject(img);
+    });
+  }
 
+//text options
+
+$scope.bold = function() {
+  if(obj.fontWeight == 'normal'){
+      obj.fontWeight = 'bold';
+   }
+   else {
+     obj.fontWeight = 'normal';
+   }
+ }
+$scope.italic = function(){
+    if(obj.fontStyle == 'normal'){
+      obj.fontStyle = 'italic';
+    }
+    else {
+      obj.fontStyle = 'normal';
+    }
+  }
+  $scope.underline = function(){
+    if(obj.textDecoration == 'normal'){
+      obj.textDecoration = 'underline';
+    }
+    else {
+      obj.textDecoration = 'normal';
+    }
+  }
+  $scope.rightalign = function(){
+    if(obj.textAlign == 'left'){
+      obj.textAlign = 'right';
+    }
+    else if (obj.textAlign == 'center') {
+      obj.textAlign = 'right';
+    }
+  }
+  $scope.leftalign = function(){
+    if(obj.textAlign == 'right'){
+      obj.textAlign = 'left';
+    }
+    else if (obj.textAlign == 'center') {
+      obj.textAlign = 'left';
+    }
+  }
+  $scope.centeralign = function(){
+    if(obj.textAlign == 'right'){
+      obj.textAlign = 'center';
+    }
+    else if (obj.textAlign == 'left') {
+      obj.textAlign = 'center';
+    }
+  }
+  $scope.fontincrease = function(){
+    obj.fontSize += 1;
+    if(1){
+      console.log(obj.fontSize);
+    return;
+    }
+  }
+  $scope.fontdecrease = function(){
+    obj.fontSize -= 1;
+  }
+
+//delete selection
+
+$scope.delete = function() {
+if($scope.canvas.getActiveGroup()){
+    $scope.canvas.getActiveGroup().forEachObject(function(o){ $scope.canvas.remove(o) });
+    $scope.canvas.discardActiveGroup().renderAll();
+  } else {
+    $scope.canvas.remove($scope.canvas.getActiveObject());
+  }
+}
+
+//chage  fontFamily
+$scope.changefont = function(){
+  obj.fontFamily = $scope.fontName;
+}
 });
