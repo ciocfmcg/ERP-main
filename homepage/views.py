fb18ee0b9314a518d1f0ecb4efc5a96a50a3d1a9
pradeep <pradeep@cioc.co.in>
2018-03-23 17:36:16 +0530

    header and footer

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,32 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request , 'index.html', {})
+    return render(request , 'index.html', {"home": True})
+
+
+def blog(request):
+    return render(request,"blog.html" , {"home" : False})
+
+def news(request):
+    return render(request,"newssection.html" , {"home" : False})
+
+def team(request):
+    return render(request,"team.html" , {"home" : False})
+
+def career(request):
+    return render(request,"career.html" , {"home" : False})
+
+def policy(request):
+    return render(request,"policy.html" , {"home" : False})
+
+def terms(request):
+    return render(request,"terms.html" , {"home" : False})
+
+def refund(request):
+    return render(request,"refund.html" , {"home" : False})
+
+def contacts(request):
+    return render(request,"contacts.html" , {"home" : False})
+
+def registration(request):
+    return render(request,"registration.html" , {"home" : False})
