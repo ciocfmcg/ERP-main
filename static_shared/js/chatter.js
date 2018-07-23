36a269f4f771d2cbf009164dee1662bb84bd9535
ankitakadam16 <ksankita16@gmail.com>
2018-07-23 16:46:27 +0530

    support

--- static_shared/js/chatter.js
+++ static_shared/js/chatter.js
@@ -1,32 +1,69 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  // alert("hello")
+
+  var connection = new autobahn.Connection({url: 'ws://162.168.1.105:8000/ws', realm: 'default'});
+
+  connection.onopen = function (session) {
+
+     console.log("session established!");
+
+     function supportChatResponse() {
+       console.log('in supportChatResponse');
+     }
+
+    session.subscribe('support.chat', supportChatResponse).then(
+      function (sub) {
+        console.log("subscribed to topic 'support.chat'");
+      },
+      function (err) {
+        console.log("failed to subscribed: " + err);
+      }
+    );
+
+  };
+
+  connection.open();
 
 
-  // var img = document.createElement("img");
-  // img.addEventListener("click", function(e) {
-  //   alert('something');
-  // }, false);
-  // img.src = "http://www.google.com/intl/en_com/images/logo_plain.png";
-  //
-  // var body = document.getElementsByTagName("BODY")[0];
-  // body.appendChild(img);
+   function getBrowserName() {
+     var name;
+     if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ){
+       name = 'Opera'
+     }else if (navigator.userAgent.indexOf("Chrome") != -1 ) {
+       name = 'Chrome'
+     }else if (navigator.userAgent.indexOf("Safari") != -1) {
+       name = 'Safari'
+     }else if (navigator.userAgent.indexOf("Firefox") != -1) {
+       name = 'Firefox'
+     }else if ((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) {
+       name = 'IE'
+     }else {
+       name = 'unknown'
+     }
+     return name
+   }
+
+   var broswer = getBrowserName()
+   var uid = document.title+'$'+broswer
+   console.log(uid);
+
+
+
 
   var body = document.getElementsByTagName("BODY")[0];
   var div = document.createElement("div");
   div.innerHTML = '<div class="chatWindow" style="height:36px;right:30px;" id="chatWindowelizabeth">'+
   '<div class="header" >'+
-    '<div class="container-fluid">'+
-      '<i class="fa fa-circle onlineStatus"></i>'+
-      '<span class="username">Online</span>'+
+    '<div class="container-fluid" style="padding:7px;" >'+
+      '<i class="fa fa-circle onlineStatus" style="color: #0b8640;font-size: 15px;" ></i>'+
+      '<span class="username" style="padding-left:7px; font-size:15px;" >Online</span>'+
       '<span class="pull-right" style="cursor:pointer; font-size:15px;"><i class="fa fa-chevron-down" id="toggle" ></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-close" id="closeChatWindow" ></i></span>'+
     '</div>'+
   '</div>'+
   '<div id="messages" class="messageView container-fluid">'+
   '</div>'+
   '<div id="footer"  class="footer">'+
-    '<div class="container-fluid input-group" id="footerInputGrp">'+
+    '<div class="container-fluid input-group" id="footerInputGrp" style="padding:10px;" >'+
       '<input id="inputText" class="form-control" style="width:100%" type="text" >'+
-      // '<input id="filePicker" style="display:none;" type="file">'+
       '<input id="filePicker" type="file" style="display:none;" accept="image/*" />'+
       '<span id="paperClip" class="input-group-addon btn btn-default " style="border:none; border-radius: 0px;"><i class="fa fa-paperclip" aria-hidden="true"></i></span>'+
       '<span id="fileName" class="input-group-addon btn btn-default" style="font-size:10px;" > filename </span>'+
@@ -38,10 +75,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   body.appendChild(div);
 
-  var toggle = true;
 
 
   (function () {
+    var toggle = true;
     document.getElementById("chatWindowelizabeth").style.height = "36px";
     document.getElementById("footer").style.display = "none";
     document.getElementById("fileName").style.display = "none";
@@ -72,16 +109,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   function messageDiv(message) {
     var col = message.sentByMe ? 'col-md-offset-2 col-md-10':'col-md-10'
-    var float = message.sentByMe ?  'right':'left'
+    var floatSide = message.sentByMe ?  'right':'left'
     var borderRad = message.sentByMe ? '20px 0px 20px 20px':'0px 20px 20px 20px'
     var bgCol = message.sentByMe ? '#dba4ae':'#72b3d4'
     var timeAgoSpan = message.sentByMe ? '<span class="text-muted pull-right" style="font-size:10px; padding-right:25px;">2 days ago</span>':'<span class="text-muted pull-left" style="font-size:10px; padding-left:25px;">5 mins ago</span>'
 
-    var msgDiv = message.msg=='' ?  '<img  src="'+message.img+'" style="width:200px;">' : '<p style="word-break: break-all !important;">'+ message.msg +'</p>'
+    var msgDiv = message.msg=='' ? '<img  src="'+ message.img +'" style="width:200px;">' : '<p style="word-break: break-all !important;">'+ message.msg +'</p>'
 
     msgHtml = '<div class="row">'+
       '<div class="'+ col +'"> '+
-        '<div style="float:'+ float +'; background-color:'+ bgCol +'; padding:10px;margin:8px; border-radius:'+ borderRad +'" >'+
+        '<div style="float:'+ floatSide +'; background-color:'+ bgCol +'; padding:10px;margin:8px; border-radius:'+ borderRad +'" >'+
           msgDiv +
         '</div>'+
       '</div>'+
@@ -92,7 +129,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     return msgHtml;
   }
 
-  pushMessges();
 
   function pushMessges() {
     for (var i = 0; i < chat.messages.length; i++) {
@@ -103,13 +139,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
     scroll();
   }
+  pushMessges();
 
   function scroll() {
     setTimeout(function () {
       var id = document.getElementById("messages");
-      console.log(id.scrollTop);
       id.scrollTop = id.scrollHeight;
-      console.log(id.scrollHeight);
     }, 200);
   }
 
@@ -120,6 +155,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   function removeFile() {
     document.getElementById('filePicker').value = "";
+
     document.getElementById("fileName").style.display = "none";
     document.getElementById("removeFile").style.display = "none";
     document.getElementById("sendFile").style.display = "none";
@@ -129,7 +165,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 
   function sendFile() {
-
     var message = {msg:'' ,  sentByMe:true , created: new Date() , img: '/static/images/career.jpg' }
     var div = document.createElement("div");
     div.innerHTML = messageDiv(message)
@@ -144,14 +179,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     document.getElementById("inputText").style.display = "";
     document.getElementById("paperClip").style.display = "";
-
   }
 
 
   document.getElementById('filePicker').onchange = function(e) {
 
-    var file = document.getElementById('filePicker')
-    console.log(file.files[0]);
+    var file = document.getElementById('filePicker');
 
     document.getElementById("inputText").style.display = "none";
     document.getElementById("paperClip").style.display = "none";
@@ -164,7 +197,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 
   var inputText = document.getElementById("inputText");
-
   inputText.addEventListener("keydown", function (e) {
       if (e.keyCode === 13) {
         if (inputText.value.length>0) {
@@ -174,6 +206,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
           var msg = document.getElementById("messages");
           msg.appendChild(div);
           scroll();
+          // connection.session.publish($scope.roomID, [$scope.lastItem], {}, {
+          //   acknowledge: true
+          // }).
+          // then(function(publication) {
+          //   console.log("Published");
+          // });
         }
         inputText.value =''
       }
